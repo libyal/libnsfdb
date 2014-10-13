026e9513360980af1bf23a3c7ace73ae1b15c599
Joachim Metz <joachim.metz@gmail.com>
2014-10-13 08:15:47 +0200

    Worked on utility scripts

--- synclibs.sh
+++ synclibs.sh
@@ -88,8 +88,8 @@ SED_SCRIPT="/^$/ {
 
 			if test ${LOCAL_LIB} = "libfvalue";
 			then
-				sed "/@LIBFGUID_CPPFLAGS@/d" -i ${LOCAL_LIB}/Makefile.am;
-				sed "/@LIBFWNT_CPPFLAGS@/d" -i ${LOCAL_LIB}/Makefile.am;
+				sed -i'~' '/@LIBFGUID_CPPFLAGS@/d' ${LOCAL_LIB}/Makefile.am;
+				sed -i'~' '/@LIBFWNT_CPPFLAGS@/d' ${LOCAL_LIB}/Makefile.am;
 			fi
 
 			rm -f ${LOCAL_LIB}/${LOCAL_LIB}.c;
