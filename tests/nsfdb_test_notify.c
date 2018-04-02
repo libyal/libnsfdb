d8e3323dea5fdd88870c6d9cfc3088ff502001c1
Joachim Metz <joachim.metz@gmail.com>
2018-04-02 10:42:57 +0200

    Applied updates

--- tests/nsfdb_test_notify.c
+++ tests/nsfdb_test_notify.c
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #endif
 
+#include "nsfdb_test_libcerror.h"
 #include "nsfdb_test_libnsfdb.h"
 #include "nsfdb_test_macros.h"
 #include "nsfdb_test_unused.h"
@@ -51,13 +52,37 @@ int nsfdb_test_notify_set_verbose(
 int nsfdb_test_notify_set_stream(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libnsfdb_notify_set_stream(
-	 NULL,
-	 NULL );
+	result = libnsfdb_notify_set_stream(
+	          NULL,
+	          &error );
+
+	NSFDB_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	NSFDB_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_set failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libnsfdb_notify_stream_open function
@@ -66,13 +91,65 @@ int nsfdb_test_notify_set_stream(
 int nsfdb_test_notify_stream_open(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libnsfdb_notify_stream_open(
-	 NULL,
-	 NULL );
+	result = libnsfdb_notify_stream_open(
+	          "notify_stream.log",
+	          &error );
+
+	NSFDB_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	NSFDB_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = libnsfdb_notify_stream_open(
+	          NULL,
+	          &error );
+
+	NSFDB_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	NSFDB_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	/* Clean up
+	 */
+	result = libnsfdb_notify_stream_close(
+	          &error );
+
+	NSFDB_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	NSFDB_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libnsfdb_notify_stream_close function
@@ -81,12 +158,36 @@ int nsfdb_test_notify_stream_open(
 int nsfdb_test_notify_stream_close(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libnsfdb_notify_stream_close(
-	 NULL );
+	result = libnsfdb_notify_stream_close(
+	          &error );
+
+	NSFDB_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	NSFDB_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_close failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* The main program
