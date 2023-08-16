09dd1f835aa677e88401dd1613d3bc8ea1d23a91
Joachim Metz <joachim.metz@gmail.com>
2023-08-16 06:13:12 +0200

    Applied updates

--- libnsfdb/libnsfdb_file.c
+++ libnsfdb/libnsfdb_file.c
@@ -503,11 +503,11 @@ int libnsfdb_file_open_file_io_handle(
      int access_flags,
      libcerror_error_t **error )
 {
-	libnsfdb_internal_file_t *internal_file = NULL;
-	static char *function                   = "libnsfdb_file_open_file_io_handle";
-	int bfio_access_flags                   = 0;
-	int file_io_handle_is_open              = 0;
-	int file_io_handle_opened_in_library    = 0;
+	libnsfdb_internal_file_t *internal_file  = NULL;
+	static char *function                    = "libnsfdb_file_open_file_io_handle";
+	uint8_t file_io_handle_opened_in_library = 0;
+	int bfio_access_flags                    = 0;
+	int file_io_handle_is_open               = 0;
 
 	if( file == NULL )
 	{
