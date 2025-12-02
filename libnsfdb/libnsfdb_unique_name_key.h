5a11c159ec06e860a08a2f2b020fc70bd32a4603
Joachim Metz <joachim.metz@gmail.com>
2025-12-02 03:50:41 +0100

    Applied updates

--- libnsfdb/libnsfdb_unique_name_key.h
+++ libnsfdb/libnsfdb_unique_name_key.h
@@ -1,7 +1,7 @@
 /*
  * Unique Name Key (UNK) functions
  *
- * Copyright (C) 2010-2024, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2025, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- libnsfdb/nsfdb_note.h
+++ libnsfdb/nsfdb_note.h
@@ -1,7 +1,7 @@
 /*
  * The note definition of a Notes Storage Facility (NSF) database file
  *
- * Copyright (C) 2010-2024, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2025, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
