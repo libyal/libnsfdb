1b58053b031eb6f281a95f0cfc37a7fa8cf1df12
Joachim Metz <joachim.metz@gmail.com>
2026-09-15 14:33:00 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libnsfdb/.libs/libnsfdb.1.dylib && test -f ./pynsfdb/.libs/pynsfdb.so
+if [ -f "${PWD}/libnsfdb/.libs/libnsfdb.1.dylib" ] && [ -f ./pynsfdb/.libs/pynsfdb.so ]
 then
-	install_name_tool -change /usr/local/lib/libnsfdb.1.dylib ${PWD}/libnsfdb/.libs/libnsfdb.1.dylib ./pynsfdb/.libs/pynsfdb.so
+    install_name_tool -change /usr/local/lib/libnsfdb.1.dylib "${PWD}/libnsfdb/.libs/libnsfdb.1.dylib" ./pynsfdb/.libs/pynsfdb.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
